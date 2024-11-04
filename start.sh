320360f463145a32993a308f069eed8e9d0506c3
lavanya gunnu <lavanyagunnu01@gmail.com>
2024-11-04 06:17:49 +0000

    instructions added

--- start.sh
+++ start.sh
@@ -6,3 +6,10 @@
 # pip install flask
 # to confirm everthing is running okay - python -m flask --version
 # flask run
+# docker build -t rest-apis-flask-python .
+# docker run -p 5005:5000 rest-apis-flask-python
+# docker compose up
+# docker compose up --build --force-recreate --no-deps web
+# docker build -t flask-smorest-api .
+# docker run -dp 5000:5000 flask-smorest-api
+# docker run -dp 5005:5000 -w /app -v "$(pwd):/app" flask-smorest-api
